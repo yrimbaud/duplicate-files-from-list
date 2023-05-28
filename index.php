0b546277e4f3aff16ad7e721c3cf5ea2ed6c902a
Yann Rimbaud <yrimbaud@gmail.com>
2023-05-28 18:32:35 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<form method="post" action="duplicateFiles.php">
+		<input type="text" name="file_name" placeholder="Nom du fichier"><br>
+		<input type="text" name="file_ext" placeholder="Extension du fichier"><br>
+		<textarea name="names" rows="10" cols="10"></textarea>
+		<input type="text" name="file_prefix" placeholder="Préfix"><br>
+		<button>Envoyer</button>
+	</form>
+</body>
+</html>
